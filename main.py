938bed44589c2773666f2ce4f558c184f18efbeb
Cameron858 <cameronwood858@googlemail.com>
2022-02-17 13:38:12 +0000

    First

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ import requests
 import pandas as pd
 
 
+def get_job_info(posting):
+    # Company name can be found in the alt text for the company's logo
+    alt_image = posting.find('div', class_="tw-flex tw-flex-col tw-w-2/5 tw-pl-4 tw-border-l-2 "
+                                           "tw-border-gray-100")
+    job_company = alt_image.a.img['alt']
+
+    # the rest of the relavent infomation can be found in the main posting
+    job = posting.find('div', class_="tw-w-3/5 tw-pr-4 tw-space-y-2")
+    job_title = job.a.text.strip()
+    job_salary = job.ul.text.splitlines()[1]
+    job_location = job.ul.text.splitlines()[2]
+    job_accepting = job.ul.text.splitlines()[3]
+    job_deadline = job.ul.text.splitlines()[-1]
+
+    return [job_title, job_company, job_salary, job_location, job_accepting, job_deadline]
+
+
 def main():
 
     data = []
@@ -27,21 +44,8 @@ def main():
     # list available jobs
     for posting in postings:
 
-        # Company name can be found in the alt text for the company's logo
-        alt_image = posting.find('div', class_="tw-flex tw-flex-col tw-w-2/5 tw-pl-4 tw-border-l-2 "
-                                               "tw-border-gray-100")
-        job_company = alt_image.a.img['alt']
-
-        # the rest of the relavent infomation can be found in the main posting
-        job = posting.find('div', class_="tw-w-3/5 tw-pr-4 tw-space-y-2")
-        job_title = job.a.text.strip()
-        job_salary = job.ul.text.splitlines()[1]
-        job_location = job.ul.text.splitlines()[2]
-        job_accepting = job.ul.text.splitlines()[3]
-        job_deadline = job.ul.text.splitlines()[-1]
-
-        # append to list
-        data.append([job_title, job_company, job_salary, job_location, job_accepting, job_deadline])
+        job = get_job_info(posting)
+        data.append(job)
 
     # append data list to pandas dataframe
     cols = ['Job title', 'Company', 'Salary', 'Location', 'Accepting', 'Deadline']
