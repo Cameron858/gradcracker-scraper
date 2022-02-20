92548ba758fc7f145236763baaab4c61c5e398de
Cameron <cameronwood858@googlemail.com>
2022-02-20 16:34:25 +0000

    Added more types of salaries from gradcracker

--- test.py
+++ test.py
@@ -3,21 +3,29 @@ import re
 
 def test():
 
+    # expected types of salaries from gradcracker
     salary_tests = [
         '£30,000',
         '€32,000',
         '£20,000-£30,000',
         '£20,000 - £30,000',
+        '£30,973 to £38,237',
         '£20,000-£30,000 rising after programme completion',
-        '£22,000 rising to £30,000 after completion of graduate scheme'
+        '£22,000 rising to £30,000 after completion of graduate scheme',
+        '£24,500 after training',
+        '£25,714 (nationally) or £32,157 (inner London)',
+        '£26,750 + £1,500 welcome bonus',
+        '£26,750 plus £1,500 welcome bonus',
+        '£27,000, rising to around £33,000 after two years',
+        '£45,000 - £50,000 salary + £2,000 - £5,000 sign on bonus',
+        'Competitive salary',
+        'Free training then £30,000',
+        'Up to £23,655'
     ]
-
-    for w in salary_tests:
-        # print(re.split('-', w))
-        pass
 
     for x in salary_tests:
         print(bool(re.match("[£][0-9]+[,][0-9]+[ |-]+[£]", x)))
+        print(re.findall(".......*-", x))
 
 
 test()
